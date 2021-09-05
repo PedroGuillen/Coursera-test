065bf7e591e7e26e667ff209d06495b81e3a3230
PedroGuillen <88627862+PedroGuillen@users.noreply.github.com>
2021-09-05 23:33:21 +0200

    Update SpeakHello.js

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -1,1 +1,10 @@
-
+yaakovGreeter.sayHello();
+johnGreeter.sayGoodBye();
+jenGreeter.sayGoodBye();
+jasonGreeter.sayGoodBye();
+paulGreeter.sayHello();
+frankGreeter.sayHello();
+larryGreeter.sayHello();
+paulaGreeter.sayHello();
+lauraGreeter.sayHello();
+jimGreeter.sayGoodBye();
